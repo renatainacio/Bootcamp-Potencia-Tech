0de72751eb36960248030773f56bc21e5665b478
renata <revazgauska@gmail.com>
2022-11-16 12:52:10 -0300

    final

--- Desafios/desafio-dio-POO/src/br/com/dio/desafio/dominio/Main.java
+++ Desafios/desafio-dio-POO/src/br/com/dio/desafio/dominio/Main.java
@@ -4,23 +4,60 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Curso curso1 = new Curso();
-        curso1.setTitulo("curso java");
-        curso1.setDescricao("descricao curso java");
-        curso1.setCargaHoraria(8);
+        Curso curso1 = new Curso(8);
+        curso1.setTitulo("Prepare-se para a Jornada");
+        curso1.setDescricao("Contém as aulas e mentoria introdutórias do Bootcamp");
 
-        Curso curso2 = new Curso();
-        curso2.setTitulo("curso js");
-        curso2.setDescricao("descricao curso js");
-        curso2.setCargaHoraria(4);
+        Curso curso2 = new Curso(13);
+        curso2.setTitulo("Princípios de Desenvolvimento de Software");
+        curso2.setDescricao("Introdução ao Pensamento Computacional e Git/GitHub");
 
-        Mentoria mentoria = new Mentoria();
-        mentoria.setTitulo("mentoria de java");
-        mentoria.setDescricao("descricao mentoria java");
-        mentoria.setData(LocalDate.now());
+        Curso curso3 = new Curso(14);
+        curso3.setTitulo("Conhecendo a Linguagem Java");
+        curso3.setDescricao("Fundamentos da Linguagem Java");
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Mentoria mentoria1 = new Mentoria(LocalDate.now());
+        mentoria1.setTitulo("Como Criar seu Plano de Estudos Usando GitHub");
+        mentoria1.setDescricao("Aprenda a utilizar essa ferramenta poderosa para organizar seus estudos!");
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Potência Tech powered by Ifood - Java Beginners");
+        bootcamp.setDescricao("O iFood chegou na DIO com uma super trilha para pessoas que querem ingressar" +
+                " no mercado tech! Trilha é voltada e focada em grupos sub-representados e pessoas de baixa renda");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(curso3);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev devRenata = new Dev("Renata");
+        devRenata.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Renata: " + devRenata.getConteudosInscritos());
+        devRenata.progredir();
+        devRenata.progredir();
+        System.out.println("***");
+        System.out.println("Conteúdos Inscritos Renata: " + devRenata.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Renata: " + devRenata.getConteudosConcluidos());
+        System.out.println("XP: " + devRenata.calcularXp());
+        double conclusao;
+        conclusao = ((double)devRenata.getConteudosConcluidos().stream().count() / (double)(devRenata.getConteudosConcluidos().stream().count()
+                + (double)devRenata.getConteudosInscritos().stream().count()));
+        System.out.println("% de Conclusão do Bootcamp: " + conclusao * 100 + "%");
+
+        System.out.println("*-----*");
+
+        Dev devCamila = new Dev("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Camila: " + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        devCamila.progredir();
+        devCamila.progredir();
+        System.out.println("***");
+        System.out.println("Conteúdos Inscritos Camila: " + devCamila.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Camila: " + devCamila.getConteudosConcluidos());
+        System.out.println("XP: " + devCamila.calcularXp());
+        conclusao = ((double)devCamila.getConteudosConcluidos().stream().count() / (double)(devCamila.getConteudosConcluidos().stream().count()
+                + (double)devCamila.getConteudosInscritos().stream().count()));
+        System.out.println("% de Conclusão do Bootcamp: " + conclusao * 100 + "%");
+
     }
 }
